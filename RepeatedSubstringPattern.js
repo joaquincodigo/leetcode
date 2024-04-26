738c1fc6f0fbdcc08c835ee732e6f5f8aaea3261
joaquincodigo <joaquin.codigo@gmail.com>
2024-04-26 12:03:17 -0300

    repeated substring

--- RepeatedSubstringPattern.js
+++ RepeatedSubstringPattern.js
@@ -2,16 +2,34 @@
  * @param {string} s
  * @return {boolean}
  */
+
 var repeatedSubstringPattern = function (s) {
-	let subStr = s[0] + s[1]
+	console.log("String:", s);
 
-	for (let i = 0; i < s.length; i++) {
-		const char = s[i];
+	let subS = ""
+	let reps
 
-
+	for (const char of s) {
+		subS += char
+		console.log("SubS:", subS)
+		reps = s.length / subS.length
+		console.log("reps:", reps)
+		if (subS.repeat(reps) == s && reps > 1) {
+			return true
+		}
 	}
+
+	return false
 };
 
+
+
+console.log("---------------------------------");
+console.log(repeatedSubstringPattern("abcd"))
+console.log("---------------------------------");
 console.log(repeatedSubstringPattern("abab"))
+console.log("---------------------------------");
 console.log(repeatedSubstringPattern("aba"))
+console.log("---------------------------------");
 console.log(repeatedSubstringPattern("aaa"))
+console.log("---------------------------------");
